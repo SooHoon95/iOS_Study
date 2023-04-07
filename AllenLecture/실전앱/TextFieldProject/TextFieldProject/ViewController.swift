fad071389bda85c49cb538e5fff8cb232a97283c
Hyeon <81559117+SooHoon95@users.noreply.github.com>
2023-04-07 15:17:49 +0900

    textField Delegate 테스트

---
+++ AllenLecture/실전앱/TextFieldProject/TextFieldProject/ViewController.swift
@@ -0,0 +1,92 @@
+//
+//  ViewController.swift
+//  TextFieldProject
+//
+//  Created by 최수훈 on 2023/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textField.delegate = self
+        
+        setUp()
+        
+    }
+
+    func setUp() {
+        view.backgroundColor = .gray
+        
+        textField.keyboardType = .emailAddress
+        textField.placeholder = "이메일 입력"
+        textField.borderStyle = .roundedRect
+        textField.clearButtonMode = .always
+        textField.returnKeyType = .default
+    }
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        
+    }
+    
+}
+
+extension ViewController : UITextFieldDelegate {
+    
+    // 텍스트 필드 입력 시작할 때 호출 ( 시작할지 말지 여부 허락 )
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print(#function)
+        return true
+    }
+    
+    // bool 리턴타입이 아니면 보통 ->
+    // 시점 -
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print(#function)
+        print("유저가 텍스트 필드의 입력을 시작했다.")
+    }
+    
+    // 텍스트 필드 글자 내용이 ( 한글자 한글자 ) 입력되거나 지워질 때 호출이 되고 (허락)
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print(string)
+        print(#function)
+        return true
+    }
+    
+    // 텍스트 필드의 엔터키가 눌러지면 다음 동작 허락할 것?
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(#function)
+        
+        if textField.text == "" {
+            textField.placeholder = "Type Something!"
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    // 텍스트 필드의 입력이 끝날 때 호출 (끝날지 말지를 허락)
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print(#function)
+        return false
+    }
+    
+    // 텍스트 필드의 입려깅 실제 끝났을 때 호출 (시점)
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        print(#function)
+        print("유저가 텍스트 필드의 입력을 끝냈다.")
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        // 지우기 버튼 작동 안되게 막아놓음
+        print(#function)
+        return true
+    }
+}
